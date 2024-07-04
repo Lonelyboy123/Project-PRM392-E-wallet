5250478be8f376e9649ce8478ee70bb60fa117e8
Dũng <dungnvhe176203@fpt.edu.vn>
2024-07-04 22:50:15 +0700

    Update ListPaymentMethod

--- app/src/main/java/com/example/project_prm392/Activity/Payment/ListPaymentMethodActivity.java
+++ app/src/main/java/com/example/project_prm392/Activity/Payment/ListPaymentMethodActivity.java
@@ -1,7 +1,12 @@
 package com.example.project_prm392.Activity.Payment;
 
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+
 import com.example.project_prm392.Activity.Base.BaseActivity;
 import com.example.project_prm392.Adapter.BankAdapter;
+import com.example.project_prm392.databinding.ActivityListPaymentMethodBinding;
 import com.example.project_prm392.entities.Banks;
 
 import java.util.ArrayList;
@@ -10,4 +15,20 @@ public class ListPaymentMethodActivity extends BaseActivity {
     ActivityListPaymentMethodBinding binding;
     private ArrayList<Banks> banksList;
     private BankAdapter bankAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityListPaymentMethodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        banksList = new ArrayList<>();
+        bankAdapter = new BankAdapter(this, banksList);
+
+        binding.rvBank.setLayoutManager(new GridLayoutManager(this, 3));
+
+        binding.rvBank.setAdapter(bankAdapter);
+//
+//        // Lấy danh sách ngân hàng từ API
+//        fetchBankList();
+//        handleButton();
+    }
 }
