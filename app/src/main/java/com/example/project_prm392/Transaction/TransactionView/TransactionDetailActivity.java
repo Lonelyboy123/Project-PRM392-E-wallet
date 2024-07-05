c2945b3651d6b3052c9663fdd0512d4710498d00
sachvo <133251939+sachvo@users.noreply.github.com>
2024-07-05 15:02:36 +0700

    list_all_transaction

---
+++ app/src/main/java/com/example/project_prm392/Transaction/TransactionView/TransactionDetailActivity.java
@@ -0,0 +1,11 @@
+package com.example.project_prm392.Transaction.TransactionView;
+
+import com.example.project_prm392.Activity.Base.BaseActivity;
+
+
+
+public class TransactionDetailActivity extends BaseActivity {
+
+
+
+}
