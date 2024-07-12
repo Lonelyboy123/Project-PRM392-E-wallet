2fdb90bd66ceaf36baf580a1bc2218f67e3ba22f
Dũng <dungnvhe176203@fpt.edu.vn>
2024-07-12 09:02:56 +0700

    Update SemesterFeeActivity

--- app/src/main/java/com/example/project_prm392/Transaction/Paying/SemesterFeeActivity.java
+++ app/src/main/java/com/example/project_prm392/Transaction/Paying/SemesterFeeActivity.java
@@ -1,4 +1,38 @@
 package com.example.project_prm392.Transaction.Paying;
 
-public class SemesterFeeActivity {
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.project_prm392.Activity.Base.BaseActivity;
+import com.example.project_prm392.databinding.ActivityTransferBinding;
+
+public class SemesterFeeActivity extends BaseActivity {
+    ActivityTransferBinding binding;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTransferBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvTransferErrAmount.setVisibility(View.GONE);
+        binding.tvTransferErrMssv.setVisibility(View.GONE);
+        binding.btnTransferContinue.setEnabled(false);
+        getBalance();
+        handleError();
+        handleButton();
+    }
+
+    private void handleButton() {
+
+    }
+
+    private void handleError() {
+
+    }
+
+    private void getBalance() {
+
+    }
 }
