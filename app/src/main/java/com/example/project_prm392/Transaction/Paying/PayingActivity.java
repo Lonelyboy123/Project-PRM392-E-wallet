b5908c50bc850099ad32c1f4de99ec39b4bf98a3
Dũng <dungnvhe176203@fpt.edu.vn>
2024-07-12 08:36:01 +0700

    Create PayingActivity
    siu

---
+++ app/src/main/java/com/example/project_prm392/Transaction/Paying/PayingActivity.java
@@ -0,0 +1,19 @@
+package com.example.project_prm392.Transaction.Paying;
+
+import android.os.Bundle;
+
+import com.example.project_prm392.Activity.Base.BaseActivity;
+import com.example.project_prm392.databinding.ActivityPayingBinding;
+
+public class PayingActivity extends BaseActivity {
+    ActivityPayingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPayingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getStudentFee();
+        handleButton();
+    }
+}
